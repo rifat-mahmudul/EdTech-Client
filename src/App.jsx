87249012ce3ee5901cba91194532d99af8f05c79
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-27 23:47:37 +0600

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="min-h-screen font-bold text-5xl flex items-center justify-center">The Storm will start from 28/01/2025</h1>
+    </>
+  )
+}
+
+export default App
