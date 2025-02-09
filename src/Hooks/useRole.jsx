95dc0e8606e209bca1340c09c37a6ef9b76ee0c2
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-02-09 09:59:12 +0600

    get user role

---
+++ src/Hooks/useRole.jsx
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure"
+
+const useRole = () => {
+
+    const axiosSecure = useAxiosSecure();
+    const {user} = useAuth();
+
+    const {data : role = "", isLoading} = useQuery({
+        queryKey : ["role"],
+        queryFn : async() => {
+            const {data} = await axiosSecure(`/users/${user?.email}`);
+            return data?.role;
+        }
+    })
+
+    return [role, isLoading]
+}
+
+export default useRole
