45dda124fc44324dbcafec648c42ffab741d2602
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-02-16 21:11:06 +0600

    get my class

--- src/page/Student/MyClasses.jsx
+++ src/page/Student/MyClasses.jsx
@@ -1,9 +1,26 @@
+import useAuth from "@/Hooks/useAuth";
+import useAxiosSecure from "@/Hooks/useAxiosSecure"
+import { useQuery } from "@tanstack/react-query";
 
 const MyClasses = () => {
+
+    const axiosSecure = useAxiosSecure();
+    const {user} = useAuth();
+
+    const {data : myCourse = []} = useQuery({
+        queryKey : ['my-course'],
+        queryFn : async () => {
+            const {data} = await axiosSecure(`/course-request/my-course/${user?.email}`)
+            return data;
+        }
+    })
+
+    console.log(myCourse)
+
   return (
-    <div>
+    <section>
       <h1>This is My Classes Route.</h1>
-    </div>
+    </section>
   )
 }
 
